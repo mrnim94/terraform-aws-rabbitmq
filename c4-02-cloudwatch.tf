6df924d72877d22812f0429a7f49a37ce49618c2
mr nim <mrnim@nimtechnology.com>
2023-04-21 16:27:12 +0700

    fix cloudwatch

--- c4-02-cloudwatch.tf
+++ c4-02-cloudwatch.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_log_group" "this" {
   for_each = var.enable_cloudwatch_logs ? toset(var.broker_cloudwatch_log_groups) : []
 
-  name              = "/aws/amazonmq/broker/${aws_mq_broker.mq.id}/${each.key}"
+  name              = "/aws/amazonmq/broker/${aws_mq_broker.rabbitmq.id}/${each.key}"
   retention_in_days = var.cloudwatch_log_group_retention_in_days
 
   tags = var.tags
